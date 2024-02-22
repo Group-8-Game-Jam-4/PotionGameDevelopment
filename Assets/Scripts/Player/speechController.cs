8b9ad76d70e95a106159fd374668f4ccac606558
Moreamix <wildcreeper@outlook.com>
2024-02-22 21:58:13 +0000

    Text System implemented
    when approaching the NPC you can now press E (as displayed on screen) and the camera will focus and zoom in. Text will then appear on the screen letter by letter. (there is currently no way to exit out of the text so I will implement that after its connected to the xml

---
+++ Assets/Scripts/Player/speechController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class speechController : MonoBehaviour
+{
+    public TextMeshProUGUI textUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator RevealText(string text)
+    {
+        Debug.Log("reveltext called");
+        text = "Hello World";
+        // Clear the text
+        textUI.text = "";
+
+        // Calculate the time interval for each letter
+        float interval = 1.0f / text.Length; // Ensure all letters are revealed within 4 seconds
+
+        // Iterate through each letter in the joke
+        for (int i = 0; i < text.Length; i++)
+        {
+            // Append the current letter to the text
+            textUI.text += text[i];
+
+            // Wait for the specified interval before revealing the next letter
+            yield return new WaitForSeconds(interval);
+        }
+
+        // Wait for the specified duration to display the revealed joke
+        yield return new WaitForSeconds(1);
+
+        // Clear the text after the display duration
+        textUI.text = "";
+    }
+}
