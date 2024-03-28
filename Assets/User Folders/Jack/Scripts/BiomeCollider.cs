f557164ccd323b2c0fcdbd102f716ccd748926c2
Jack R <thereapercountdown@outlook.com>
2024-03-28 23:54:19 +0000

    biome gate switches scenes based on string input

--- Assets/User Folders/Jack/Scripts/BiomeCollider.cs
+++ Assets/User Folders/Jack/Scripts/BiomeCollider.cs
@@ -1,27 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BiomeCollider : MonoBehaviour
 {
     bool interactable = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string scenename;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.E) && interactable == true)
         {
-            Debug.Log("attempting to switch scenes");
+            SceneManager.LoadScene(scenename);
             interactable = false;
         }
     }
 
+    //allows player interaction with biome trigger if they enter its collision
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
@@ -31,6 +28,7 @@ public class BiomeCollider : MonoBehaviour
         }
     }
 
+    //disallows player interaction with biome trigger if they exit its collision
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Player")
