33c8a2c1cad8794bad7c80279e9152b34f2a8366
Moreamix <68817644+Moreamix@users.noreply.github.com>
2024-03-28 02:46:56 +0000

    Inteactions v1.1
    - Object now floats to player before being collected
    - Small trail from object to show movement
    - Particle effect to show collection of item

--- Assets/Scripts/Interactions/itemCollect.cs
+++ Assets/Scripts/Interactions/itemCollect.cs
@@ -1,38 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Interactions : MonoBehaviour
 {
+    public GameObject player;
     public GameObject hintText;
-    public bool inRange;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float moveSpeed = 5f;
+    private bool isPickedUp = false;
+    private bool inRange;
+    private bool hasInteracted = false; // Flag to track whether the object has been picked up and moved towards the player
+    public ParticleSystem burstEffect;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && inRange == true)
+        if (Input.GetKeyDown(KeyCode.E) && inRange && !isPickedUp)
         {
+            isPickedUp = true;
+            hasInteracted = true; // Set the flag when the object is picked up
+        }
+
+        if (isPickedUp)
+        {
+            Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+            transform.position += directionToPlayer * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player && hasInteracted && isPickedUp)
+        {
+            // Set the item's position to the player's position
+            transform.position = player.transform.position;
+            burstEffect.Play();
+            // Destroy the item immediately
             Destroy(gameObject);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        hintText.SetActive(true);
-        inRange = true;
-
+        if (collision.gameObject == player && !isPickedUp)
+        {
+            hintText.SetActive(true);
+            inRange = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        hintText.SetActive(false);
-        inRange = false;
+        if (collision.gameObject == player)
+        {
+            hintText.SetActive(false);
+            inRange = false;
+        }
     }
 }
