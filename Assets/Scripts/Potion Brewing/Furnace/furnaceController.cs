d133b908cc8ab0658ccacfc65b0ab5286a1658e0
Moreamix <wildcreeper@outlook.com>
2024-04-21 19:29:38 +0100

    Furnace connected to Inv

--- Assets/Scripts/Potion Brewing/Furnace/furnaceController.cs
+++ Assets/Scripts/Potion Brewing/Furnace/furnaceController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,7 +7,7 @@ public class furnaceController : MonoBehaviour
     public int furnaceLvl;
     public Image furnaceImage;
     public Sprite[] furnaceSprites; // Array to hold UI images
-
+    public WorkstationSystem workStationScript;
     private int clicksNeeded;
 
     void Start()
@@ -16,30 +17,37 @@ public class furnaceController : MonoBehaviour
         UpdateFurnaceSprite();
     }
 
-    public void progressLvl()
+    public void Update()
     {
-        clicksNeeded--;
-        if (clicksNeeded <= 0)
+        if (furnaceLvl == 4)
         {
-            // Increment furnace level and reset clicks needed
-            furnaceLvl++;
-            SetClicksNeeded();
-            UpdateFurnaceSprite();
+            workStationScript.BrewPotion();
         }
     }
+        public void progressLvl()
+        {
+            clicksNeeded--;
+            if (clicksNeeded <= 0 && workStationScript.readyToBrew == true)
+            {
+                // Increment furnace level and reset clicks needed
+                furnaceLvl++;
+                SetClicksNeeded();
+                UpdateFurnaceSprite();
+            }
+        }
 
-    void SetClicksNeeded()
-    {
-        // Generate random number between 1 and 3 for the next progression
-        clicksNeeded = Random.Range(1, 4);
-    }
+        void SetClicksNeeded()
+        {
+            // Generate random number between 1 and 3 for the next progression
+            clicksNeeded = Random.Range(1, 4);
+        }
 
-    void UpdateFurnaceSprite()
-    {
-        // Make sure furnaceLvl does not exceed the number of sprites
-        furnaceLvl = Mathf.Clamp(furnaceLvl, 0, furnaceSprites.Length - 1);
+        void UpdateFurnaceSprite()
+        {
+            // Make sure furnaceLvl does not exceed the number of sprites
+            furnaceLvl = Mathf.Clamp(furnaceLvl, 0, furnaceSprites.Length - 1);
 
-        // Update the sprite to the one corresponding to the furnaceLvl
-        furnaceImage.sprite = furnaceSprites[furnaceLvl];
-    }
+            // Update the sprite to the one corresponding to the furnaceLvl
+            furnaceImage.sprite = furnaceSprites[furnaceLvl];
+        }
 }
