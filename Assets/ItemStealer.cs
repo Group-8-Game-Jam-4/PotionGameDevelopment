7ed53d3662b79d57290a18315810f644f358f253
Alfie <87566534+AlfieRichards@users.noreply.github.com>
2024-04-26 16:09:21 +0100

    Update ItemStealer.cs

--- Assets/ItemStealer.cs
+++ Assets/ItemStealer.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class ItemStealer : MonoBehaviour
 {
+    // so were going to need an inventory for this, probably a total inventory as that will just sort of work
     // Start is called before the first frame update
-    void Start()
+    public ContainerInventory cart;
+    public int minStolenItems;
+    public int maxStolenItems;
+    public ContainerInventory goblinShop;
+    
+    public void Steal()
     {
-        
-    }
+        // work out how many items to steal
+        int itemsToSteal = Random.Range(minStolenItems, maxStolenItems);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(cart.formattedInventory.Count > itemsToSteal)
+        {
+            for(int i = 0; i < itemsToSteal; i++)
+            {
+                // get random one from the inv
+                int itemToSteal = Random.Range(0, cart.formattedInventory.Count);
+                string[] item = cart.formattedInventory[itemsToSteal];
+                
+                // take the item and put it in our one
+                cart.inventory.TakeItem(item[0], 1);
+                goblinShop.inventory.AddItem(item[0], 1);
+            }
+        }
     }
 }
