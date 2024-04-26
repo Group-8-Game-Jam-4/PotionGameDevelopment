1c30b8c4406f8520be21439df87cf8cd6d359bc3
Jack R <thereapercountdown@outlook.com>
2024-04-26 16:06:32 +0100

    added comments yippers

--- Assets/User Folders/Jack/Scripts/Time System/TimeSystem.cs
+++ Assets/User Folders/Jack/Scripts/Time System/TimeSystem.cs
@@ -11,35 +11,49 @@ public class TimeSystem : MonoBehaviour
 
     private void Start()
     {
+        //runs time increment every second
         InvokeRepeating("TimeIncrement", 0f, 1f);
     }
 
     void TimeIncrement()
     {
+        //increases minutes passed by whatever time increment is set to (currently 1)
         minutesPassed += timeIncrement;
 
+        //if 60 minutes have passed (60 seconds irl if increment is set to 1)
         if (minutesPassed >= 60)
         {
+            //increment hours by 1
             hoursPassed += 1;
+
+            //reset minutes
             minutesPassed = 0;
+
+            //run hours has passed
             HourHasPassed();
         }
 
+        //if 24 hours have passed (24 minutes irl if increment is set to 1 i think)
         if (hoursPassed >= 24)
         {
+            //increment days by 1
             daysPassed += 1;
+
+            //reset hours
             hoursPassed = 0;
+
+            //run days has passed
             DayHasPassed();
         }
     }
 
     void HourHasPassed()
     {
-        Debug.Log("hour has passed");
+        //put shit here
     }
 
     void DayHasPassed()
     {
-        Debug.Log("day has passed");
+        //put shit here
     }
 }
