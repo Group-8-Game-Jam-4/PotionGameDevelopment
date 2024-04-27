4a4d41723fa4df00d84d6c4bd8dc7bfba051d454
Jack R <thereapercountdown@outlook.com>
2024-04-27 19:37:09 +0100

    Made the cauldron function, no crafting function

--- Assets/User Folders/Jack/Scripts/Cauldron/Bubbles Moving.cs
+++ Assets/User Folders/Jack/Scripts/Cauldron/Bubbles Moving.cs
@@ -1,43 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BubblesMoving : MonoBehaviour
 {
     public GameObject bubble;
-    int stopDistance;
+    public GameObject bubbleSprite;
+    double stopDistance;
+    float startDistance;
     float moveSpeed;
-    bool registered = false;
+    float scaleStart;
+    float scaleEnd;
+    public bool registered = false;
     // Start is called before the first frame update
     void Start()
     {
-        stopDistance = Random.Range(281, 366);
-        moveSpeed = Random.Range(0.3f, 0.6f);
+        //get the start distance and starting scale so that we can reset the bubbles
+        startDistance = transform.position.y;
+        scaleStart = transform.localScale.x;
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && (registered == false))
+        //temp just for testing, gonna be triggered by crafting later
+        if(registered == false)
         {
+            //registered = true so it doesnt loop
             registered = true;
-            invokeMovement();
+
+            //randomize the distance the bubbles will travel, their scale and their move speed
+            //stopDistance = Random.Range(300, 350);
+
+            //stopDistance = Screen.height/1.9;
+            stopDistance = Random.Range((float)(Screen.height / 1.9), (float)(Screen.height / 2.05));
+            moveSpeed = Random.Range(0.2f, 0.4f);
+            scaleEnd = Random.Range(0.25f, 0.35f);
+
+            //set all bubbles to active
+            //this might not actually be needed (the for each loop) cuz this script is on all the bubbles anyway
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+
+            //move all bubbles
+            bubble.gameObject.transform.localScale = new Vector2(scaleEnd, scaleEnd);
+
+            //this might be highly fucking inefficient but im honestly not sure
+            // if this like chokes the players fps then ill try think of an alterative
+            //
+            //repeat the move bubbles function
+            InvokeRepeating("MoveBubbles", 0f, 0.005f);
         }
     }
 
-    void invokeMovement()
+    void MoveBubbles()
     {
-        InvokeRepeating("moveBubbles", 0f, 0.005f);
-    }
-
-    void moveBubbles()
-    {
+        //if we're not at the stopping distance
         if (transform.position.y < stopDistance)
         {
+            //move the bubble
             transform.position = new Vector2(transform.position.x, transform.position.y + moveSpeed + Time.deltaTime);
         }
 
-        if (transform.position.y >= stopDistance) 
+        //if we are at the stopping distance
+        if (transform.position.y >= stopDistance)
         {
+            //cancel this function so we dont like explode someones computer
+            CauldronAnimationController bubbleAnimation = bubbleSprite.GetComponent<CauldronAnimationController>();
+            bubbleAnimation.StartAnimationAuto();
             CancelInvoke();
         }
     }
--- Assets/User Folders/Jack/Scripts/Cauldron/Cauldron Bubbles.cs
+++ Assets/User Folders/Jack/Scripts/Cauldron/Cauldron Bubbles.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class CauldronBubbles : MonoBehaviour
 {
     public int bubblesPopped;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] bubbleObject;
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        //if bubbles popped is greater than or equal to 5
+        if (bubblesPopped >= 5)
+        {
+            //run reset bubbles
+            for (int i = 0; i < bubbleObject.Length; i++)
+            {
+                //get bubbles
+                BubblesMoving bubble = bubbleObject[i].GetComponent<BubblesMoving>();
 
-    public void onClickPop()
-    {
-        bubblesPopped += 1;
-        Debug.Log(bubblesPopped);
+                //set registered as true so they stop spawning
+                bubble.registered = true;
+
+                //stop the movement of the bubbles
+                bubble.CancelInvoke();
+
+                //delete the bubbles
+                bubbleObject[i].transform.GetChild(0).gameObject.SetActive(false);
+
+                //hooking up to crafting goes here
+            }
+
+            //reset bubbles popped
+            bubblesPopped = 0;
+        }
     }
 }
